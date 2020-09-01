20aaf24a518349e37bd55f9e88a685a7e282053c
womstar7 <womstar7@gmail.com>
2020-09-02 00:58:02 +0900

    2nd commit

---
+++ Day05/src/Day05.java
@@ -0,0 +1,14 @@
+public class Day05 {
+
+    public static void main(String[] args) {
+
+        System.out.println(6 + 2); //8
+        System.out.println(6 - 2); // 4
+        System.out.println(6 * 2); // 12
+        System.out.println(6 / 2); //3
+
+        System.out.println(Math.PI); //
+        System.out.println(Math.floor(Math.PI));  //3
+        System.out.println(Math.ceil(Math.PI));  //4
+    }
+}
