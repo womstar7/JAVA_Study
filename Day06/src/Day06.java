98b791ef7d89b66f700c37f203ca3d3011aeeb52
womstar7 <womstar7@gmail.com>
2020-09-03 21:08:23 +0900

    6th commit

---
+++ Day06/src/Day06.java
@@ -0,0 +1,20 @@
+public class Day06 {
+
+    public static void main(String[] args) {
+
+        //Character VS String
+        System.out.println("Hello Would"); // String
+        System.out.println('H');  // Character
+        System.out.println("H");
+
+        System.out.println("Hello"
+                + "World");
+
+        //New line
+        System.out.println("hello \nWorld");
+
+        // escape
+        System.out.println("Hello \"World\""); // Hello "World"
+
+    }
+}
