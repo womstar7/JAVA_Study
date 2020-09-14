e5d08ce971de840196518a560447dad2d9dfc51c
womstar7 <womstar7@gmail.com>
2020-09-14 23:51:04 +0900

    6th commit

---
+++ Day 07/src/Day07.java
@@ -0,0 +1,9 @@
+public class Day07 {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello Would".length()); //11
+        System.out.println("Hello, [[[name]]] ... bye.".replace("[[[name]]]", "duru"));
+
+    }
+}
