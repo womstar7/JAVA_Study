2cad4dedc3426053600bf204a07e0af2c52105c1
womstar7 <womstar7@gmail.com>
2020-09-16 23:54:29 +0900

    8th commit

---
+++ Day 08/src/Day08.java
@@ -0,0 +1,15 @@
+public class Day08 {
+
+    public static void main(String[] args) {
+
+        int a = 1; //Number -> integer ... -2, -1, 0, 1, 2
+        System.out.println(a);
+
+        double b = 1.1; //real number -> double ... -2.0, -1.0
+        System.out.println(b);
+
+        String c = "Hello World";
+        System.out.println(c);
+
+    }
+}
