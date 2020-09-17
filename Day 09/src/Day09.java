c4e2752d2bfb1680bd2a4751d5468ddaf484b6ee
womstar7 <womstar7@gmail.com>
2020-09-17 23:18:39 +0900

    9th commit

---
+++ Day 09/src/Day09.java
@@ -0,0 +1,15 @@
+public class Day09 {
+
+    public static void main(String[] args) {
+
+        String name = "한별";
+        System.out.println("Hello, "+name+" ... "+name+" ... emif ... bye");
+        // 근데 이 경우 다량으로 메일을 발송하려고 하면 변수 "" 안에 들어가는 내용을 일일이 수기입 할 수 없는 문제는 어떻게 해야하지? !
+
+        double VAT = 10.0;
+        System.out.println(VAT);
+
+    }
+
+
+}
