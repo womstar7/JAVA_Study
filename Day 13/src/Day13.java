a763531ae6bb55150b9e886431922475b42b0481
womstar7 <womstar7@gmail.com>
2020-09-26 10:07:39 +0900

    13rd commit

--- Day 13/src/Day13.java
+++ Day 13/src/Day13.java
@@ -1,12 +1,15 @@
+import org.opentutorials.iot.DimmingLights;
 import org.opentutorials.iot.Elevator;
 import org.opentutorials.iot.Lighting;
 import org.opentutorials.iot.Security;
 
-public class  Day12 {
+public class  Day13 {
 
+    // paramter, 매개변수
     public static void main(String[] args) {
 
-        String id = "JAVA APT 507";
+        String id = args[0];
+        String bright = args[1];
 
         // Elevator call
         Elevator myElevator = new Elevator(id);
@@ -17,12 +20,17 @@ public class  Day12 {
         mySecurity.off();
 
         // Light on
-        Lighting hallLamp = new Lighting(id+"/ Hall Lamp");
+        Lighting hallLamp = new Lighting(id + "/ Hall Lamp");
         hallLamp.on();
 
-        Lighting floorLamp = new Lighting(id+" / floorLamp");
+        Lighting floorLamp = new Lighting(id + " / floorLamp");
         floorLamp.on();
+
+        DimmingLights moodLamp = new DimmingLights(id+" moodLamp");
+        moodLamp.setBright(Double.parseDouble(bright));
+        moodLamp.on();
 
 
     }
+
 }
